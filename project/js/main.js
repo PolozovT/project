a04e4b42cfc562fcf8c33892bba0bd30b595503f
t.polozov <39823227+PolozovT@users.noreply.github.com>
2018-10-05 15:49:56 +0300

    new files

---
+++ project/js/main.js
@@ -0,0 +1,143 @@
+/*var greeting="Hi";
+var chisloIzStroki=parseInt("10011", 2)
+
+//alert(greeting);
+document.write(chisloIzStroki);
+ 
+ var chislo=chisloIzStroki.toString(2);
+ document.write("<br />");
+ document.write(chislo);
+
+//rounding
+document.write("<br />");
+document.write(12.222^0);
+document.write("<br />");
+document.write(0^12.222);*/
+
+/*var str="hello";
+
+if (~str.indexOf("lo")){
+	alert("found!");
+}
+
+
+function isInteger(num) {
+  return (num ^ 0) === num;
+}
+
+document.write("<br />");
+document.write(isInteger(1.3));
+
+var name = prompt("Ваше имя?", "");
+alert("hello, "+name );
+
+var age = prompt('Введите возраст', 0);
+
+var message = (age < 3) ? "Hello, malysh":
+(age < 18) ? "Hi":
+(age < 100) ? "Good afternoon!":
+(age < 200) ? "HELLO, YOU SO COOL!!!":
+"WAT???";
+
+alert(message);
+
+var namejs = prompt("Официальное название js?", "");
+
+if (namejs == "EcmaScript") alert("Good")
+	else alert("no...");
+*/
+/*
+var admin = prompt("Кто пришел?", "");
+
+if (admin == "админ") {
+	var password = prompt("Пароль?", "");
+	if (password == "чв") alert("preveD")
+		else if (password == null) alert("вход отменен");
+		else alert("ПAKA");
+}
+    else if (admin == null) alert ("вход отменен");
+	else alert("я вас не знаю");
+
+result = (a+b < 4) ? "malo": "mnogo";*/
+/*
+var director = prompt('Введите login', "");
+
+var message = (director == "vasya") ? alert("privet"):
+(director == "director") ? alert("zrdaste"):
+(director == "") ? alert("net logina"):
+alert("");
+
+var ages=prompt("vozrast?","");
+
+if (!(ages>=14&&ages<=90)) alert("klevo");
+
+*/
+
+/*
+var i = 2;
+for (; i<=10; i++){
+	if(i%2==0){
+	alert(i);
+}
+}
+*/
+
+/*
+var i = 0;
+while ( i < 3) {
+  alert( "номер " + i + "!" );
+  i++;
+}
+*/
+
+/*
+var ch;
+do{
+	ch=prompt("vvedi chislo > 100", "");
+}
+while (ch<100 || ch==null);
+*/
+
+
+/*
+next:
+for (var i = 1; i<10; i++){
+	for (var j=2; j<i; j++){
+		if (i%j==0) continue next;}
+		alert(i);
+}
+*/
+
+/*
+var browser = prompt("Введите ваш браузер");
+
+if (browser=='IE') {
+    alert( 'О, да у вас IE!' );}
+    else if (browser=='Chrome' || browser=='Firefox' || browser=='Safari' || browser=='Opera'){
+    	alert( 'Да, и эти браузеры мы поддерживаем' );
+    }
+    else{
+    alert( 'Мы надеемся, что и в вашем браузере все ок!' );}
+*/
+
+/*
+var a = +prompt('a?', '');
+
+switch (a) {
+  case 0:
+   alert( 0 );
+   break;
+  case 1: 
+   alert( 1 );
+   break;
+  case 2:
+  case 3:
+   alert( '2,3' );
+   break;
+  default:
+   alert("xz");
+}
+*/
+
+
+
